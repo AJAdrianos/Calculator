6f62e490f7f4674fd3fae4c178907c03085725df
AJ <aj.lotterboy@gmail.com>
2024-09-11 19:41:09 +0200

    Main layout of app completed

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-"" 
+const reservedChar = 
+    [ 
+        "(", ")", "/",
+        "x", "-", "+",
+        ".", "%",
+        "9", "8", "7", 
+        "6", "5", "4", 
+        "3", "2", "1", 
+        "0",
+    ];
+
+let expression = "";
+
+
+// click button, add value to the expression
+// press equal, evaluate the expression 
+// double for, for checking if values arent in the reservedChar array
+
+function addValueToExpression(value) {
+    console.log(value);
+};
+
+function clearExpression(){
+    expression = "";
+}
